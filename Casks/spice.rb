9820b1540bbfcbeda7ba3f99734f917f03852ae7
Karl <karl.kwong@gmail.com>
2026-04-12 11:27:19 +0200

    Bump spice to 2.2.6

---
+++ Casks/spice.rb
@@ -0,0 +1,22 @@
+cask "spice" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "2.2.6"
+  sha256 "7b63f6bfd4c824150588dd165a1a1090f47cc4b6aa9059034f536dc498037aef"
+
+  url "https://github.com/dixieflatline76/Spice/releases/download/v#{version}/Spice-#{version}-macos-#{arch}.dmg"
+  name "Spice"
+  desc "Highly-concurrent, plugin-driven desktop environment engine"
+  homepage "https://spicebox.dev"
+
+  auto_updates true
+
+  app "Spice.app"
+  app "Spice Wallpaper Manager Extension.app"
+
+  zap trash: [
+    "~/Library/Application Support/Spice",
+    "~/Library/Preferences/com.dixieflatline76.spice.plist",
+    "~/Library/Saved Application State/com.dixieflatline76.spice.savedState",
+  ]
+end
